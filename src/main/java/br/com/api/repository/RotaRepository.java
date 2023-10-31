083757f3363064005e829a09c811a859da8e0eb9
yonix42 <joao.staub42@gmail.com>
2023-10-31 20:36:45 -0300

    Removendo o Rota antigo

--- src/main/java/br/com/api/repository/RotaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.repository;
-
-import br.com.api.model.Rota;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RotaRepository extends JpaRepository<Rota, Integer> {
-}
